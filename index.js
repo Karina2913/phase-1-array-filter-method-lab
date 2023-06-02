610d368a0109a5146b0a2d5ed6ed7a302d124388
Karina Pliego <karinapliegovergara@gmail.com>
2023-06-02 10:42:04 -0500

    Completed Array Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    return drivers.filter(function (drivers) {
+        return drivers.toLowerCase() === string.toLowerCase()
+    });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (drivers) {
+        return drivers.toLowerCase().substring(0, string.length) === string.toLowerCase();
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function (drivers) {
+        return drivers.name === string;
+    })
+}
